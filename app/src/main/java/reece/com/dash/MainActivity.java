7faaf014dfe6633a78b59bca17cae47c9b7185c3
Reece Spragg <>
2019-10-21 12:33:11 +1300

    -Doesn't build- Layout commits for navigation.

--- app/src/main/java/reece/com/dash/MainActivity.java
+++ app/src/main/java/reece/com/dash/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MotionEvent;
 
 import reece.com.dash.ui.main.onBoarding1_fragment;
 import reece.com.dash.ui.main.onBoarding2_fragment;
+import reece.com.dash.ui.main.onBoarding3_fragment;
 
 /*
     Drive for Mobile App Assignment
@@ -15,7 +16,7 @@ import reece.com.dash.ui.main.onBoarding2_fragment;
     - Add Animation
 
     - Create CameraLayout
-    - Camera Permissions
+    - CameraActivity Permissions
     - Opt 1. Recording, double tap starts recording until long tapped
 
     - Gallery for viewing recordings/info. Time/Date
@@ -25,13 +26,12 @@ import reece.com.dash.ui.main.onBoarding2_fragment;
     - Speed in info.
     - Music Control.
 
-
-
  */
 
 
 
 public class MainActivity extends AppCompatActivity {
+    private int currentFragmentFeature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +46,42 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, onBoarding2_fragment.newInstance())
-                .commitNow();
+        changeFragment('R');
         return super.onTouchEvent(event);
+    }
+
+    public void changeFragment(char direction){
+        switch (direction){
+            case 'L':
+                currentFragmentFeature = currentFragmentFeature - 1;
+                if(currentFragmentFeature<0){
+                    currentFragmentFeature = 0;
+                }
+                break;
+            case 'R':
+                currentFragmentFeature = currentFragmentFeature + 1;
+                if(currentFragmentFeature>2){
+                    currentFragmentFeature = 2;
+                }
+                break;
+        }
+        switch (currentFragmentFeature){
+            case 0:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, onBoarding1_fragment.newInstance())
+                        .commitNow();
+                break;
+            case 1:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, onBoarding2_fragment.newInstance())
+                        .commitNow();
+                break;
+            case 2:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container, onBoarding3_fragment.newInstance())
+                        .commitNow();
+                break;
+        }
+
     }
 }
--- app/src/main/java/reece/com/dash/ui/main/onBoarding3_fragment.java
+++ app/src/main/java/reece/com/dash/ui/main/onBoarding3_fragment.java
@@ -1,112 +1,55 @@
 package reece.com.dash.ui.main;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import reece.com.dash.CameraActivity;
 import reece.com.dash.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link onBoarding3_fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link onBoarding3_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class onBoarding3_fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MainViewModel mViewModel;
 
-    private OnFragmentInteractionListener mListener;
-
-    public onBoarding3_fragment() {
-        // Required empty public constructor
+    public static onBoarding3_fragment newInstance() {
+        return new onBoarding3_fragment();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment onBoarding3_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static onBoarding3_fragment newInstance(String param1, String param2) {
-        onBoarding3_fragment fragment = new onBoarding3_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.onboarding3_fragment, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        ImageView swipeButton = getActivity().findViewById(R.id.imageViewSwipe);
+        swipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoCamera();
+            }
+        });
+        // TODO: Use the ViewModel
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    private void gotoCamera(){
+        Intent intent = new Intent(getActivity(), CameraActivity.class);
+        startActivity(intent);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+
 }
